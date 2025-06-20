4e5edc9dfa0c50111e0a9c0e53330fbefe5e8f71
Shree-dev-0104 <shree.kumar.dev@gmail.com>
2025-06-20 20:53:32 +0530

    Connected Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit/features/auth/screens/login_screen.dart';
+import 'package:reddit/firebase_options.dart';
 import 'package:reddit/theme/pallete.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase here if needed
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
